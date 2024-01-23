5168df8b5590dd4bf4cd12be16c9b625dd71df8a
Vaibhav-Kumar1312 <vaibhav.kumar458@gmail.com>
2024-01-24 00:54:18 +0530

    added createProject.ejs

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,3 +1,31 @@
+const Project = require("../models/project.js");
 exports.homePage = function homePage(req, res) {
   return res.render("home", { title: "Home" });
+};
+
+exports.newProject = async function newProject(req, res) {
+  const allProjects = await Project.find({});
+  return res.render("createProject", {
+    title: "Create New Project",
+    allProjects: allProjects,
+  });
+};
+
+exports.createNewProject = async function createNewProject(req, res) {
+  console.log(req.body.name);
+  console.log(req.body.description);
+  console.log(req.body.author);
+  let newProject = await Project.create({
+    name: req.body.name,
+    description: req.body.description,
+    author: req.body.author,
+  });
+
+  res.redirect("back");
+};
+
+exports.projectDetail = function projectDetail(req, res) {
+  console.log(req.params.id);
+  console.log(req.params.name);
+  return res.redirect("back");
 };
--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,8 @@ const router = express.Router();
 const homeController = require("../controllers/homeController.js");
 
 router.get("/", homeController.homePage);
+router.get("/newProject", homeController.newProject);
+router.post("/newProject/create", homeController.createNewProject);
+router.get("/projectDetail/:id", homeController.projectDetail);
 
 module.exports = router;
